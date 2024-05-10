661d20c4a170f2d55d83028ec554c3888dbb2fab
GTHC-009 <144570398+GTHC-009@users.noreply.github.com>
2024-05-09 21:55:35 -0700

    Create script.js

---
+++ Grupo 113 - Info/script.js
@@ -0,0 +1,7 @@
+function mostrarinfo () {
+    document.getElementById("info-completa").style.display = "flex";
+}
+
+function ocultarinfo () {
+    document.getElementById("info-completa").style.display = "none";
+}
